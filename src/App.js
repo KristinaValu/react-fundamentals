ab3292b458b1ddee7e02ffd936d9da2f585026a2
KristinaValu <chriskristina@yahoo.com>
2023-02-23 13:51:33 +0200

    First React Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+const FirstReactComponent = () => {
+  return <p>I am new here</p>;
+};
+
 function App() {
   return (
     <>
       <h1>Hello World</h1>
       <p>You look beautiful</p>
+      <FirstReactComponent />
     </>
   );
 }
