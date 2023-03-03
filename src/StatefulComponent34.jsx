d6ab58675ab8bd4c37e07dd38b645dfe823a6ea4
KristinaValu <chriskristina@yahoo.com>
2023-03-03 21:40:18 +0200

    Stateful Component

--- src/StatefulComponent34.jsx
+++ src/StatefulComponent34.jsx
@@ -3,12 +3,12 @@ import { useState } from "react";
 export default function StatefulComponent34() {
   const [count, setCount] = useState(0);
   function increment() {
-    setCount(count + 1);
+    setCount(prevCount => prevCount + 1);
   }
   function decrement() {
-    let positive = count > 0;
+    const positive = count > 0;
     if (positive) {
-      setCount(count - 1);
+      setCount(prevCount => prevCount - 1);
     }
   }
   return (
